cb41de37040f7bdc78b99acc931b0a69c6de7eb3
MUHAMMED-RAMSHAL <shaluramshal@gmail.com>
2025-01-07 18:58:52 +0300

    07/01/2025

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -96,7 +96,7 @@ const Cart = () => {
           <div className="w-full text-end">
             <button
               onClick={() => navigate("/place-order")}
-              className="bg-black text-white text-sm my-8 px-8 py-3 rounded"
+              className="bg-black text-white text-sm my-8 px-8 py-3 rounded font-semibold"
             >
               PROCEED TO CHECKOUT
             </button>
